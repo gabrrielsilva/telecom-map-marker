c3f8970ed8d8cad987a3006b5e70ac9c2b04b0a2
gabrrielsilva <gabrielsilvaev@gmail.com>
2023-02-16 19:18:57 -0300

    first commit

---
+++ styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export { styles };
+
